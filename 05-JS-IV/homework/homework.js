783afa7c23669cee16f579012bba17dc8433689c
gonzalop0210 <gonzalop0210@gmail.com>
2022-11-17 10:22:49 -0500

    Homework IV terminado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -54,13 +54,23 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  let nuevoObjeto = {
+    nombre: nombre,
+    email: email,
+    password: password
+  }
+  return nuevoObjeto;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (usuario.email) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -68,6 +78,11 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto[propiedad]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function verificarPassword (usuario, password) {
@@ -75,12 +90,19 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (usuario.password == password) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo (usuario, nuevoAmigo) {
@@ -88,6 +110,8 @@ function agregarAmigo (usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium (usuarios) {
@@ -96,6 +120,12 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i]) {
+      usuarios[i].esPremium = true;
+    }
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario (usuario) {
@@ -105,6 +135,12 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var cont = 0;
+  var total = 0;
+  for (let i = 0; i < usuario.posts.length; i++) {
+    cont += usuario.posts[i].likes;
+  }
+  return cont;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
@@ -117,7 +153,12 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto["calcularPrecioDescuento"] =  function () {
+    let rpta = 0;
+    rpta = producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+    return rpta;
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
